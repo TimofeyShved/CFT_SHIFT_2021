f0df4673390714ab34be0f25f10ea707a7ffb66b
timofeya123 <timofeya123@inbox.ru>
2021-07-05 14:31:35 +0600

    UserAdd

--- src/main/java/com/example/CFT_SHIFT_2021/controller/UserController.java
+++ src/main/java/com/example/CFT_SHIFT_2021/controller/UserController.java
@@ -7,28 +7,41 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController              // так как порт 8080 был занят, я его поменял на http://localhost:8070/user/ ------- не знаю, как у вас там всё устроено ¯\_(ツ)_/¯
-@RequestMapping("/user")
+@RequestMapping
 public class UserController {
 
     @Autowired
     private UserCRUD userCRUD; // создаём интерфейс для взаимодействия с бд
 
-    @PostMapping("/") // создать
+    @PostMapping("/user") // создать
     public ResponseEntity registration(@RequestBody UserEntity user){
         try {
+            if ((userCRUD.findUserEntityByFirstName(user.getFirstName())!=null)&&(userCRUD.findUserEntityBylastName(user.getLastName())!=null)){
+                return  ResponseEntity.badRequest().body(
+                        "{"+System.lineSeparator()+
+                        "code: USER_EXISTS"+System.lineSeparator()+
+                        "}"
+                );
+            }
             userCRUD.save(user);
-            return ResponseEntity.ok("Save user");
+            return ResponseEntity.ok(
+                    "{"+System.lineSeparator()+
+                    "userId: "+user.getUserId()+System.lineSeparator()+
+                    "firstName: "+user.getFirstName()+System.lineSeparator()+
+                    "lastName: "+user.getLastName()+System.lineSeparator()+
+                    "}"
+            );
         }catch (Exception e){
-            return  ResponseEntity.badRequest().body("error");
+            return  ResponseEntity.badRequest().body("code: ERROR");
         }
     }
 
-    @GetMapping("/") // взять
+    @GetMapping("/users") // взять
     public ResponseEntity getUser(){
         try {
             return ResponseEntity.ok("RUN SERVER, ok!");
         }catch (Exception e){
-            return  ResponseEntity.badRequest().body("error");
+            return  ResponseEntity.badRequest().body("code: USER_NOT_FOUND");
         }
     }
 }
--- src/main/java/com/example/CFT_SHIFT_2021/repository/UserCRUD.java
+++ src/main/java/com/example/CFT_SHIFT_2021/repository/UserCRUD.java
@@ -4,4 +4,6 @@ import com.example.CFT_SHIFT_2021.entity.UserEntity;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserCRUD extends CrudRepository<UserEntity, Long> { // наследуемый интерфейс для изменения данных в бд
+    UserEntity findUserEntityByFirstName(String firstName);
+    UserEntity findUserEntityBylastName(String lastName);
 }
